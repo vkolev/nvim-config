8ff6225b2f89b909ae27a0360e1909c4dd751a31
Vladimir Kolev <vladimir.r.kolev@gmail.com>
2023-09-08 10:43:37 +0200

    Add current configuration

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Change window size
+map("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
---
+++ lua/plugins/extra/lang/python.lua
@@ -0,0 +1,123 @@
+return {
+    -- Add `pyright` to mason
+    {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+            -- vim.list_extend(opts.ensure_installed, { "pyright", "black", "ruff-lsp", "ruff" })
+            vim.list_extend(opts.ensure_installed, {
+                "pyright",
+                "black",
+                "ruff",
+            })
+        end,
+    },
+
+    -- Setup adapters as nvim-dap dependencies
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "mfussenegger/nvim-dap-python",
+      -- stylua: ignore
+      keys = {
+        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
+        { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class" },
+      },
+            config = function()
+                local path = require("mason-registry").get_package("debugpy"):get_install_path()
+                require("dap-python").setup(path .. "/venv/bin/python")
+            end,
+        },
+    },
+
+    -- Add `python` debugger to mason DAP to auto-install
+    -- Not absolutely necessary to declare adapter in `ensure_installed`, since `mason-nvim-dap`
+    -- has `automatic-install = true` in LazyVim by default and it automatically installs adapters
+    -- that are are set up (via dap) but not yet installed. Might as well skip the lines below as
+    -- a whole.
+
+    -- Add which-key namespace for Python debugging
+    {
+        "folke/which-key.nvim",
+        optional = true,
+        opts = {
+            defaults = {
+                ["<leader>dP"] = { name = "+Python" },
+            },
+        },
+    },
+
+    -- Setup `neotest`
+    {
+        "nvim-neotest/neotest",
+        optional = true,
+        dependencies = {
+            "nvim-neotest/neotest-python",
+        },
+        opts = {
+            adapters = {
+                ["neotest-python"] = {
+                    -- Here you can specify the settings for the adapter, i.e.
+                    runner = "pytest",
+                    -- python = ".venv/bin/python",
+                },
+            },
+        },
+    },
+
+    -- Add `server` and setup lspconfig
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {},
+        opts = {
+            servers = {
+                pyright = {},
+                ruff_lsp = {},
+                -- jedi_language_server = {},
+            },
+            setup = {
+                ruff_lsp = function()
+                    require("lazyvim.util").on_attach(function(client, _)
+                        if client.name == "ruff_lsp" then
+                            -- Disable hover in favor of Pyright
+                            client.server_capabilities.hoverProvider = false
+                        end
+                    end)
+                end,
+                pyright = function()
+                    require("lazyvim.util").on_attach(function(client, _)
+                        if client.name == "pyright" then
+                            -- disable hover in favor of jedi-language-server
+                            client.server_capabilities.hoverProvider = false
+                        end
+                    end)
+                end,
+            },
+        },
+    },
+
+    -- Setup null-ls with `black`
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        opts = function(_, opts)
+            local nls = require("null-ls")
+            opts.sources = vim.list_extend(opts.sources, {
+                -- Order of formatters matters. They are used in order of appearance.
+                nls.builtins.formatting.ruff,
+                nls.builtins.formatting.black,
+                -- nls.builtins.formatting.black.with({
+                --   extra_args = { "--preview" },
+                -- }),
+                -- nls.builtins.diagnostics.ruff,
+            })
+        end,
+    },
+
+    -- For selecting virtual envs
+    {
+        "linux-cultist/venv-selector.nvim",
+        dependencies = { "sharkdp/fd" },
+        cmd = "VenvSelect",
+        opts = {},
+        keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+    },
+}
